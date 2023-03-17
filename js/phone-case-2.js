e04c40095385782e168201a4632c06834c95c18e
ImranRH321 <imranrh247@gmail.com>
2023-03-17 21:29:19 +0600

    n

--- js/phone-case-2.js
+++ js/phone-case-2.js
@@ -2,56 +2,42 @@
 document
   .getElementById("phone_Plus_button")
   .addEventListener("click", function () {
-    //    input incase or decrees
     const inputValueUpdate = idSelectorValueTextChange("phoneInputFlied", true);
-
-    // input value or price multiply =
     priceIncaseOrDecrees("phonePriceText", inputValueUpdate, 59);
     //  ====================
     const phonePrice1 = textConvertPrice("priceText");
     // phone 2
     const phonePrice2 = textConvertPrice("phonePriceText");
-    // console.log('phonePrice2',phonePrice2);
     const totalPrice = phonePrice1 + phonePrice2;
-    // subTotal text
+
     setTextElementId("subTotal", totalPrice);
     //
     const amountDiscount = (totalPrice * 0.1).toFixed(2);
     setTextElementId("discountPriceText", amountDiscount);
-
-    //  total price
     const finalResultPrice = totalPrice - amountDiscount;
-
-    // total text
     setTextElementId("fainalPrice", finalResultPrice);
   });
-// 2 phone cuss minus
+// ('=======Minus=======');
 document
   .getElementById("phone_manus_button")
   .addEventListener("click", function () {
-    //    input incase or decrees
     const inputValueUpdate = idSelectorValueTextChange(
       "phoneInputFlied",
       false
     );
-    // input value or price multiply =
     priceIncaseOrDecrees("phonePriceText", inputValueUpdate, 59);
     //  ====================
 
     const phonePrice1 = textConvertPrice("priceText");
-    // phone 2
+
     const phonePrice2 = textConvertPrice("phonePriceText");
-    // console.log('phonePrice2',phonePrice2);
     const totalPrice = phonePrice1 + phonePrice2;
-    // subTotal text
     setTextElementId("subTotal", totalPrice);
     //
     const amountDiscount = (totalPrice * 0.1).toFixed(2);
     setTextElementId("discountPriceText", amountDiscount);
 
-    //  total price
     const finalResultPrice = totalPrice - amountDiscount;
 
-    // total text
     setTextElementId("fainalPrice", finalResultPrice);
   });
